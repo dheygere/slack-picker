c3b18ec9af6137e80bd37f7753ff66fba69993bf
Thomas d'Heygere <thomas.d-heygere@mythalesgroup.io>
2018-10-04 22:39:01 +0200

    Switch to application/x-www-form-urlencoded input

--- index.js
+++ index.js
@@ -1,9 +1,6 @@
 const url = require('url')
-const {json} = require('micro');
 
 module.exports = async request => {
-  const data = await json(request);
-//  console.log(data.text)
-
-  return 'Youpi ' + data.text
+  const query = url.parse(request.url, true).query
+  return 'Youpi ' + query.text
 }
